e1123fba2950d65bbfff47bccfb48aae96c16e31
ecarnovsky <ecarnovsky@gmail.com>
2024-03-15 18:23:59 -0400

    Added the canvas

---
+++ built/main.js
@@ -0,0 +1,7 @@
+var paused = true;
+document.getElementById('start-pause-btn').addEventListener('click', togglePause);
+var canvas = document.querySelector('canvas');
+canvas.height = 730;
+canvas.width = 730;
+function togglePause() {
+}
---
+++ ts/main.ts
@@ -0,0 +1,12 @@
+let paused = true
+
+document.getElementById('start-pause-btn').addEventListener('click', togglePause)
+
+const canvas = document.querySelector('canvas')
+canvas.height = 730
+canvas.width = 730 
+
+
+function togglePause(){
+    
+}
